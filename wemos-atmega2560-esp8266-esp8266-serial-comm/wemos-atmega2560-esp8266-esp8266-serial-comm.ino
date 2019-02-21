e8d6f76d5de0dcb049efd3b081c74dd177d07d36
damaniac82 <laszlo.merczel@gmail.com>
2019-02-22 00:20:41 +0100

    Added more stats to be collected

--- wemos-atmega2560-esp8266-esp8266-serial-comm/wemos-atmega2560-esp8266-esp8266-serial-comm.ino
+++ wemos-atmega2560-esp8266-esp8266-serial-comm/wemos-atmega2560-esp8266-esp8266-serial-comm.ino
@@ -26,19 +26,19 @@ void connectIfNeeded() {
   }
 }
 
-JsonObject& getJson(const char * url) {
+JsonObject& getJson(const char * url, int bufferSize) {
   
   // get data over rest - repeat this for whatever endpoints you want to fetch and display
   http.begin(url);
   http.GET();
   String payload = http.getString();
-  DynamicJsonBuffer jsonBuffer(200);
+  DynamicJsonBuffer jsonBuffer(bufferSize);
   return jsonBuffer.parseObject(payload);
 }
 
-char * getUpTime(const char * url) {
+char* getUpTime(const char * url) {
   
-  JsonObject& root = getJson(url);
+  JsonObject& root = getJson(url, 200);
   if (!root.success()) {
     return ">Service unavailable!<";
   }
@@ -48,23 +48,54 @@ char * getUpTime(const char * url) {
   int uptime_mins = (uptime_secs_t - uptime_hours * 60 * 60) / 60;
   int uptime_secs = (uptime_secs_t - uptime_hours * 60 * 60 - uptime_mins * 60);
   
-  char buf[256];
+  char * buf = (char*)malloc(sizeof(char) * 32);
   sprintf(buf, ">Uptime: %0d:%0d:%0d<", uptime_hours, uptime_mins, uptime_secs);
   return buf;
 }
 
-char * getCPUUsage(const char * url) {
+char* getCPUUsage(const char * url) {
   
-  JsonObject& root = getJson(url);
+  JsonObject& root = getJson(url, 455);
   if (!root.success()) {
     return ">Service unavailable!<";
   }
 
-  // TODO: Parse and return!
-  float value = 0.0;
+  char float_tmp[7];
+  JsonArray& data_0 = root["data"][0];
+  dtostrf((float)data_0[4] + (float)data_0[5] + (float)data_0[6] + (float)data_0[7] + (float)data_0[8] + (float)data_0[9], 3, 2, float_tmp);
   
-  char buf[32];
-  sprintf(buf, ">CPU: %00d%%<", value);
+  char * buf = (char*)malloc(sizeof(char) * 32);
+  
+  strcpy(buf, ">CPU: ");
+  strcat(buf, float_tmp);
+  strcat(buf, "%<");
+
+  return buf;
+}
+
+char *labels[4] = {"RAM Free:   ", "RAM Used:   ", "RAM Cached: ", "RAM Buffers:"};
+char* getRAMUsage(const char * url) {
+  
+  JsonObject& root = getJson(url, 455);
+  if (!root.success()) {
+    return ">Service unavailable!<";
+  }
+
+  JsonArray& data_0 = root["data"][0];
+
+  char * buf = (char*)malloc(sizeof(char) * 256);
+  strcpy(buf, "");
+  for (int i = 1; i <= 4; i++) {
+    char float_tmp[8];
+    float ram = data_0[i];  
+    dtostrf(ram, 4, 2, float_tmp);
+    strcat(buf, ">");
+    strcat(buf, labels[i-1]);
+    strcat(buf, " ");
+    strcat(buf, float_tmp);
+    strcat(buf, "%<\n");
+  }
+
   return buf;
 }
 
@@ -74,7 +105,8 @@ void loop() {
   connectIfNeeded();
 
   Serial.println(getUpTime(NETDATA_URL_UPTIME));
-  //Serial.println(getCPUUsage(NETDATA_URL_CPU));
+  Serial.println(getCPUUsage(NETDATA_URL_CPU));
+  Serial.println(getRAMUsage(NETDATA_URL_RAM));
   
-  delay(1000);
+  delay(2000);
 }
