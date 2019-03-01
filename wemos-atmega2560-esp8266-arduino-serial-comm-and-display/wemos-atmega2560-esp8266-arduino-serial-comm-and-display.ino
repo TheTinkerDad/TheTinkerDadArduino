68e034a2619e25abd92e5ca582457a893cff325d
damaniac82 <laszlo.merczel@gmail.com>
2019-03-01 09:41:26 +0100

    Code cleanup

--- wemos-atmega2560-esp8266-arduino-serial-comm-and-display/wemos-atmega2560-esp8266-arduino-serial-comm-and-display.ino
+++ wemos-atmega2560-esp8266-arduino-serial-comm-and-display/wemos-atmega2560-esp8266-arduino-serial-comm-and-display.ino
@@ -1,20 +1,12 @@
 #include <MCUFRIEND_kbv.h>
-#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_GFX.h>
 
 #define FONT_HEIGHT 16
 #define FONT_WIDTH 12
-
 #define MAIN_BUF_SIZE 256
 #define READ_BUF_SIZE 64
-
 #define BLACK   0x0000
-#define BLUE    0x001F
-#define RED     0xF800
 #define GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
 
 MCUFRIEND_kbv tft;
 int16_t font_size, lines; 
@@ -26,8 +18,7 @@ void setup() {
   Serial3.begin(9600);
   
   tft.reset();
-  uint16_t id = tft.readID();
-  tft.begin(id);
+  tft.begin(tft.readID());
   tft.setRotation(1);
 
   font_size = FONT_HEIGHT / 8;
@@ -36,18 +27,12 @@ void setup() {
   tft.fillScreen(BLACK);
   tft.setTextColor(GREEN, BLACK);
   tft.setTextSize(font_size);
-
   tft.setCursor(0, 0);
-  tft.setTextColor(GREEN); 
   tft.println("Awaiting data...");
 
   memset(mainbuffer, 0, MAIN_BUF_SIZE);
 }
 
-bool startsWith(const char *str, const char *pre) {
-    return strncmp(pre, str, strlen(pre)) == 0;
-}
- 
 void loop(void) {
 
   int readBufSize = tft.width() / FONT_WIDTH;
@@ -61,18 +46,12 @@ void loop(void) {
 
     // Process the data we've received, even if it's partial
     for (int i = 0; i < batchSize; i++){
-      // New data line
-      if (buffer[i] == '>') {
+      if (buffer[i] == '>') {                 // New data block
         memset(mainbuffer, 0, MAIN_BUF_SIZE);
-
-      // Finished data line
-      } else if (buffer[i] == '<') {
+      } else if (buffer[i] == '<') {          // Finished data block
         tft.setCursor(0,0);
-        tft.setTextColor(GREEN, BLACK);
         tft.println(mainbuffer);
-
-      // Within data line
-      } else {
+      } else {                                // Within data block
         mainbuffer[strlen(mainbuffer)] = buffer[i];
       }
     }
